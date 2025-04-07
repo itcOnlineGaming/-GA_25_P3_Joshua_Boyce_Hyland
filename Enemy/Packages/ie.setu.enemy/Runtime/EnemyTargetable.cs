f5ebe6f4b6b0c06cd34a3eaf745debb6ab28bdcf
Josh <joshua.boycehyland15@gmail.com>
2025-04-07 08:18:49 +0100

    git troubles tying to fix

--- Enemy/Packages/ie.setu.enemy/Runtime/EnemyTargetable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class EnemyTargetable : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    public bool attackable = true;
-   public abstract void TakeDamage(float t_damage);
-}
